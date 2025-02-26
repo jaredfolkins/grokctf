fa0ff2d063932429339ba5326742c6a55db760cc
Jared Folkins <jfolkins@gmail.com>
2025-02-25 16:41:41 -0800

    updated port

--- main.go
+++ main.go
@@ -55,8 +55,8 @@ func main() {
 	http.HandleFunc("/userquery", userQueryHandler) // Renamed from /idor
 
 	// Start server
-	log.Println("Server starting on :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Println("Server starting on :8081")
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
